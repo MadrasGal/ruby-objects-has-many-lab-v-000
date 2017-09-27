6cf23f7ce9c125ffd3692b387eaee00380e14b62
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-09-27 04:13:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,10 @@ class Song
   end
 
   def artist_name
-    if (self.artist.name == '')
-        return nil
-      else
-
-        self.artist.name
+    if self.artist 
+      self.artist.name
+    else
+      nil
     end
   end
 
